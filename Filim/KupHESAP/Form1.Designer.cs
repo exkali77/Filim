8aec86a4aba2311eb725086e2023ec9703cbfd78
Kerem Şenses <68199514+exkali77@users.noreply.github.com>
2022-11-07 21:51:02 +0300

    Filim Projesi
    Bilgisayardan Filim İzleme

---
+++ Filim/KupHESAP/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace KupHESAP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.kATEGORİToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aksyionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.romantikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.korkuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bilimkurguToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.renklerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.maviToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kırmızıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.siyahToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hızlıErişimToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakımızdaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.madMaxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.aILoveYouToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.şeytanınAvukatıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pesencerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kATEGORİToolStripMenuItem,
+            this.renklerToolStripMenuItem,
+            this.hızlıErişimToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // kATEGORİToolStripMenuItem
+            // 
+            this.kATEGORİToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aksyionToolStripMenuItem,
+            this.romantikToolStripMenuItem,
+            this.korkuToolStripMenuItem,
+            this.bilimkurguToolStripMenuItem});
+            this.kATEGORİToolStripMenuItem.Name = "kATEGORİToolStripMenuItem";
+            this.kATEGORİToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.kATEGORİToolStripMenuItem.Text = "KATEGORİ";
+            // 
+            // aksyionToolStripMenuItem
+            // 
+            this.aksyionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.madMaxToolStripMenuItem});
+            this.aksyionToolStripMenuItem.Name = "aksyionToolStripMenuItem";
+            this.aksyionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aksyionToolStripMenuItem.Text = "Aksyion";
+            // 
+            // romantikToolStripMenuItem
+            // 
+            this.romantikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aILoveYouToolStripMenuItem});
+            this.romantikToolStripMenuItem.Name = "romantikToolStripMenuItem";
+            this.romantikToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.romantikToolStripMenuItem.Text = "Romantik";
+            // 
+            // korkuToolStripMenuItem
+            // 
+            this.korkuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.şeytanınAvukatıToolStripMenuItem});
+            this.korkuToolStripMenuItem.Name = "korkuToolStripMenuItem";
+            this.korkuToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.korkuToolStripMenuItem.Text = "Korku";
+            // 
+            // bilimkurguToolStripMenuItem
+            // 
+            this.bilimkurguToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pesencerToolStripMenuItem});
+            this.bilimkurguToolStripMenuItem.Name = "bilimkurguToolStripMenuItem";
+            this.bilimkurguToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bilimkurguToolStripMenuItem.Text = "Bilimkurgu";
+            // 
+            // renklerToolStripMenuItem
+            // 
+            this.renklerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.maviToolStripMenuItem,
+            this.kırmızıToolStripMenuItem,
+            this.siyahToolStripMenuItem});
+            this.renklerToolStripMenuItem.Name = "renklerToolStripMenuItem";
+            this.renklerToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.renklerToolStripMenuItem.Text = "Renkler";
+            // 
+            // maviToolStripMenuItem
+            // 
+            this.maviToolStripMenuItem.Name = "maviToolStripMenuItem";
+            this.maviToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.maviToolStripMenuItem.Text = "Mavi";
+            this.maviToolStripMenuItem.Click += new System.EventHandler(this.maviToolStripMenuItem_Click);
+            // 
+            // kırmızıToolStripMenuItem
+            // 
+            this.kırmızıToolStripMenuItem.Name = "kırmızıToolStripMenuItem";
+            this.kırmızıToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.kırmızıToolStripMenuItem.Text = "Kırmızı";
+            this.kırmızıToolStripMenuItem.Click += new System.EventHandler(this.kırmızıToolStripMenuItem_Click);
+            // 
+            // siyahToolStripMenuItem
+            // 
+            this.siyahToolStripMenuItem.Name = "siyahToolStripMenuItem";
+            this.siyahToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.siyahToolStripMenuItem.Text = "Siyah";
+            this.siyahToolStripMenuItem.Click += new System.EventHandler(this.siyahToolStripMenuItem_Click);
+            // 
+            // hızlıErişimToolStripMenuItem
+            // 
+            this.hızlıErişimToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hakımızdaToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.hızlıErişimToolStripMenuItem.Name = "hızlıErişimToolStripMenuItem";
+            this.hızlıErişimToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.hızlıErişimToolStripMenuItem.Text = "Hızlı Erişim";
+            // 
+            // hakımızdaToolStripMenuItem
+            // 
+            this.hakımızdaToolStripMenuItem.Name = "hakımızdaToolStripMenuItem";
+            this.hakımızdaToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.hakımızdaToolStripMenuItem.Text = "Hakımızda";
+            this.hakımızdaToolStripMenuItem.Click += new System.EventHandler(this.hakımızdaToolStripMenuItem_Click);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // madMaxToolStripMenuItem
+            // 
+            this.madMaxToolStripMenuItem.Name = "madMaxToolStripMenuItem";
+            this.madMaxToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.madMaxToolStripMenuItem.Text = "Mad Max";
+            this.madMaxToolStripMenuItem.Click += new System.EventHandler(this.madMaxToolStripMenuItem_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 24);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(800, 426);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // aILoveYouToolStripMenuItem
+            // 
+            this.aILoveYouToolStripMenuItem.Name = "aILoveYouToolStripMenuItem";
+            this.aILoveYouToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aILoveYouToolStripMenuItem.Text = "aI Love You";
+            this.aILoveYouToolStripMenuItem.Click += new System.EventHandler(this.aILoveYouToolStripMenuItem_Click);
+            // 
+            // şeytanınAvukatıToolStripMenuItem
+            // 
+            this.şeytanınAvukatıToolStripMenuItem.Name = "şeytanınAvukatıToolStripMenuItem";
+            this.şeytanınAvukatıToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.şeytanınAvukatıToolStripMenuItem.Text = "Şeytanın Avukatı";
+            this.şeytanınAvukatıToolStripMenuItem.Click += new System.EventHandler(this.şeytanınAvukatıToolStripMenuItem_Click);
+            // 
+            // pesencerToolStripMenuItem
+            // 
+            this.pesencerToolStripMenuItem.Name = "pesencerToolStripMenuItem";
+            this.pesencerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pesencerToolStripMenuItem.Text = "pesencer";
+            this.pesencerToolStripMenuItem.Click += new System.EventHandler(this.pesencerToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kATEGORİToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aksyionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem romantikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem korkuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bilimkurguToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem renklerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem maviToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kırmızıToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem siyahToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hızlıErişimToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hakımızdaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem madMaxToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStripMenuItem aILoveYouToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem şeytanınAvukatıToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pesencerToolStripMenuItem;
+    }
+}
+
---
+++ Filim/KupHESAP/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KupHESAP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void maviToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Blue;
+        }
+
+        private void kırmızıToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+        }
+
+        private void siyahToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Black;
+        }
+
+        private void hakımızdaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("KEREM ŞENSES", "bİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void madMaxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/watch?v=A2F6YwU1OEs");
+        }
+
+        private void aILoveYouToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/watch?v=kdEExeQ8D8U");
+        }
+
+        private void şeytanınAvukatıToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/watch?v=jQ3uMl2J4m4");
+        }
+
+        private void pesencerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/watch?v=mKAfeLyC14Q");
+        }
+    }
+}
